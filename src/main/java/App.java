fb854ae5ace630a3747d9f1301dca575015b5686
rh <ryanjharvey88@gmail.com>
2016-04-23 20:19:54 -0700

    Finalized styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,7 @@ import static spark.Spark.*;
 
 public class App{
   public static void main(String[] args){
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
